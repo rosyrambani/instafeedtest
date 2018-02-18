5ba8e44285a70de372acc41c0d88d6d3a0c72a2f
rosyrambani <rosys9319@gmail.com>
2018-02-17 22:47:31 -0500

    edits

--- custom.js
+++ custom.js
@@ -2,14 +2,16 @@ $( document ).ready(function() {
 
 	var userFeed = new Instafeed({
 		get: 'user',
-		userId: '1584366392',
+		userId: '',
 		limit: 20,
 		resolution: 'standard_resolution',
-		accessToken: '1584366392.1677ed0.5a4f5b55779144c8ac2b9279c8221f32',
+		accessToken: '',
 		sortBy: 'most-recent',
 		template: '<div class="col-lg-4 gallery"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
 	});
 	userFeed.run();
-
+	
+	/* get userid from https://smashballoon.com/instagram-feed/find-instagram-user-id/?username=juanvelezfitness&349zxd17610jjzl=4 */
+	/* get accessToken from http://instagram.pixelunion.net/  */
 
 });
